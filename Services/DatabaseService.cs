884c60639336875f6a59c0f0b138a68679e78596
Cristiano Leite <cristianosleite@hotmail.com>
2025-12-12 16:40:51 -0300

    Refatora criação de receitas padrão e remove métodos obsoletos
    Refatora a geração das receitas padrão usando um laço for, simplifica a declaração da lista defaultRecipes e melhora a legibilidade em SaveRecipeAsync. Remove os métodos SaveRecipesAsync e GetNextRecipeIdAsync, que não são mais necessários.

--- Services/DatabaseService.cs
+++ Services/DatabaseService.cs
@@ -32,19 +32,19 @@ namespace UAUIngleza_plc.Services
             var recipeCount = await _database.Table<Recipe>().CountAsync();
             if (recipeCount == 0)
             {
-                var defaultRecipes = new List<Recipe>
+                List<Recipe> defaultRecipes = [];
+
+                for (int i = 1; i <= 10; i++)
                 {
-                    new Recipe { Id = 1, Name = "Receita 1", Bottles = 0 },
-                    new Recipe { Id = 2, Name = "Receita 2", Bottles = 0 },
-                    new Recipe { Id = 3, Name = "Receita 3", Bottles = 0 },
-                    new Recipe { Id = 4, Name = "Receita 4", Bottles = 0 },
-                    new Recipe { Id = 5, Name = "Receita 5", Bottles = 0 },
-                    new Recipe { Id = 6, Name = "Receita 6", Bottles = 0 },
-                    new Recipe { Id = 7, Name = "Receita 7", Bottles = 0 },
-                    new Recipe { Id = 8, Name = "Receita 8", Bottles = 0 },
-                    new Recipe { Id = 9, Name = "Receita 9", Bottles = 0 },
-                    new Recipe { Id = 10, Name = "Receita 10", Bottles = 0 },
-                };
+                    var recipe = new Recipe
+                    {
+                        Id = i,
+                        Name = $"Receita {i}",
+                        Bottles = 0,
+                    };
+
+                    defaultRecipes.Add(recipe);
+                }
                 await _database.InsertAllAsync(defaultRecipes);
             }
         }
@@ -84,7 +84,8 @@ namespace UAUIngleza_plc.Services
         {
             await InitAsync();
 
-            var existingRecipe = await _database!.Table<Recipe>()
+            var existingRecipe = await _database!
+                .Table<Recipe>()
                 .FirstOrDefaultAsync(r => r.Id == recipe.Id);
 
             if (existingRecipe != null)
@@ -97,33 +98,10 @@ namespace UAUIngleza_plc.Services
             }
         }
 
-        public async Task SaveRecipesAsync(List<Recipe> recipes)
-        {
-            await InitAsync();
-
-            foreach (var recipe in recipes)
-            {
-                await SaveRecipeAsync(recipe);
-            }
-        }
-
         public async Task DeleteRecipeAsync(Recipe recipe)
         {
             await InitAsync();
             await _database!.DeleteAsync(recipe);
-        }
-
-        public async Task<int> GetNextRecipeIdAsync()
-        {
-            await InitAsync();
-            var recipes = await _database!.Table<Recipe>()
-                .OrderByDescending(r => r.Id)
-                .ToListAsync();
-            
-            if (recipes.Count == 0)
-                return 1;
-            
-            return recipes[0].Id + 1;
         }
     }
 }

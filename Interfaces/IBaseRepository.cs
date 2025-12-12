e592a387a593b8098162c00daf1c503239034c45
Cristiano Leite <cristianosleite@hotmail.com>
2025-12-12 16:52:07 -0300

    Refatora IBaseRepository para melhor legibilidade
    Remove diretivas using desnecessárias e ajusta a formatação dos métodos genéricos na interface IBaseRepository, melhorando a organização e leitura do código sem alterar sua lógica.

--- Interfaces/IBaseRepository.cs
+++ Interfaces/IBaseRepository.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using UAUIngleza_plc.Models;
-using UAUIngleza_plc.Services;
-
-namespace UAUIngleza_plc.Interfaces
+﻿namespace UAUIngleza_plc.Interfaces
 {
     public interface IBaseRepository
     {
-        Task<List<T>> GetAsync<T>() where T : class;
-        Task<T?> GetOneAsync<T>(int id) where T : class;
-        Task SaveAsync<T>(T type) where T : class;
-        Task UpdateAsync<T>(T type) where T : class;
-        Task DeleteAsync<T>(T type) where T : class;
+        Task<List<T>> GetAsync<T>()
+            where T : class;
+        Task<T?> GetOneAsync<T>(int id)
+            where T : class;
+        Task SaveAsync<T>(T type)
+            where T : class;
+        Task UpdateAsync<T>(T type)
+            where T : class;
+        Task DeleteAsync<T>(T type)
+            where T : class;
     }
 }

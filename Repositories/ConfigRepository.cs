2b4f924e0931119f8754b58302fef614ed010896
Cristiano Leite <cristianosleite@hotmail.com>
2025-12-12 16:42:52 -0300

    Refatora ConfigRepository com construtor primário
    Refatora a classe ConfigRepository para utilizar construtor primário,
    simplificando a injeção de dependência do DatabaseService. Remove
    usings desnecessários, melhora a formatação e clareza das exceções.
    Sem alterações na lógica de negócio.

--- Repositories/ConfigRepository.cs
+++ Repositories/ConfigRepository.cs
@@ -1,35 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UAUIngleza_plc.Interfaces;
+﻿using UAUIngleza_plc.Interfaces;
 using UAUIngleza_plc.Services;
 using SystemConfig = UAUIngleza_plc.Models.SystemConfiguration;
 
 namespace UAUIngleza_plc.Repositories
 {
-    public class ConfigRepository : IConfigRepository
+    public class ConfigRepository(DatabaseService databaseService) : IConfigRepository
     {
-        private readonly DatabaseService _databaseService;
+        private readonly DatabaseService _databaseService = databaseService;
 
-        public ConfigRepository(DatabaseService databaseService)
-        {
-            _databaseService = databaseService;
-        }
-
-        public async Task<List<T>> GetAsync<T>() where T : class
+        public async Task<List<T>> GetAsync<T>()
+            where T : class
         {
             if (typeof(T) == typeof(SystemConfig))
             {
                 await _databaseService.InitAsync();
                 var config = await _databaseService.GetConfigurationAsync();
-                return new List<T> { config as T ?? throw new InvalidOperationException("Configuração não encontrada") };
+                return
+                [
+                    config as T
+                        ?? throw new InvalidOperationException("Configuração não encontrada"),
+                ];
             }
-            throw new NotSupportedException($"Tipo {typeof(T).Name} não suportado por ConfigRepository.");
+            throw new NotSupportedException(
+                $"Tipo {typeof(T).Name} não suportado por ConfigRepository."
+            );
         }
 
-        public async Task<T?> GetOneAsync<T>(int id) where T : class
+        public async Task<T?> GetOneAsync<T>(int id)
+            where T : class
         {
             if (typeof(T) == typeof(SystemConfig))
             {
@@ -37,10 +35,13 @@ namespace UAUIngleza_plc.Repositories
                 var config = await _databaseService.GetConfigurationAsync();
                 return config as T;
             }
-            throw new NotSupportedException($"Tipo {typeof(T).Name} não suportado por ConfigRepository.");
+            throw new NotSupportedException(
+                $"Tipo {typeof(T).Name} não suportado por ConfigRepository."
+            );
         }
 
-        public async Task SaveAsync<T>(T type) where T : class
+        public async Task SaveAsync<T>(T type)
+            where T : class
         {
             if (type is SystemConfig config)
             {
@@ -48,10 +49,13 @@ namespace UAUIngleza_plc.Repositories
                 await _databaseService.SaveConfigurationAsync(config);
                 return;
             }
-            throw new NotSupportedException($"Tipo {typeof(T).Name} não suportado por ConfigRepository.");
+            throw new NotSupportedException(
+                $"Tipo {typeof(T).Name} não suportado por ConfigRepository."
+            );
         }
 
-        public async Task UpdateAsync<T>(T type) where T : class
+        public async Task UpdateAsync<T>(T type)
+            where T : class
         {
             if (type is SystemConfig config)
             {
@@ -59,10 +63,13 @@ namespace UAUIngleza_plc.Repositories
                 await _databaseService.SaveConfigurationAsync(config);
                 return;
             }
-            throw new NotSupportedException($"Tipo {typeof(T).Name} não suportado por ConfigRepository.");
+            throw new NotSupportedException(
+                $"Tipo {typeof(T).Name} não suportado por ConfigRepository."
+            );
         }
 
-        public async Task DeleteAsync<T>(T type) where T : class
+        public async Task DeleteAsync<T>(T type)
+            where T : class
         {
             throw new NotSupportedException("Não é possível deletar a configuração do sistema.");
         }
